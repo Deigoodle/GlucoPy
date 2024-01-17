c1c5e79fd10e111a0922a85f8c55ad6ea00dbff0
Deigoodle <diego.nicocastillo@gmail.com>
2024-01-17 15:14:50 -0300

    data()

--- glucopy/data/data.py
+++ glucopy/data/data.py
@@ -1,2 +1,27 @@
+# 3rd party
+import pandas as pd
+
 def data(dataset : str = 'prueba 1.csv'):
-    
+    '''
+    Glucopy includes a few datasets to test the package, this function downloads
+    and returns one of them
+
+    The following datasets are available:
+    * prueba 1.csv
+    * prueba 2.csv
+
+    Parameters
+    ----------
+    dataset : str, optional
+        Name of the dataset to download, by default 'prueba 1.csv'
+
+    Returns
+    -------
+    Gframe
+        Gframe object
+    '''
+    dataset = dataset.lower()
+
+    path = 'https://raw.githubusercontent.com/Deigoodle/GlucoPy/main/data/'
+
+    return pd.read_csv(path + dataset)
