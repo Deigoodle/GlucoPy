22c5b56312f72da2679e89a89dd68d52cf128458
Deigoodle <diego.nicocastillo@gmail.com>
2024-01-23 11:37:37 -0300

    setup update

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 from setuptools import setup
 
-setup(
-    name="glucopy",
-    version="0.1",
-    install_requires=[
+# Dependencies
+requirements = [
         "numpy==1.26.2",
         "pandas==2.1.4",
         "plotly==5.18.0",
         "scipy==1.11.4",
         "neurokit2==0.2.7",
         "requests==2.31.0", # neurokit dependency
-    ],
-    extras_require={
-        "optional": [
-            "openpyxl==3.1.2",
-            "xlrd==2.0.1",
+        "openpyxl==3.1.2", # for excel export
+        "xlrd==2.0.1", # for excel import
         ]
-    }
+
+setup(
+    name="glucopy",
+    version="0.1",
+    install_requires=requirements,
 )
